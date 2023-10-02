1ff61a42ed2057e1def79f37e04c5ab7f44b7194
Mafciejewicz <melonik0.9846@gmail.com>
2023-10-02 23:09:28 +0200

    Switch styled-components to mantine

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,14 +1,18 @@
 import { PropsWithChildren } from 'react';
-import { StyledComponentsRegistry } from '~/lib/StyledComponentsRegistry';
-import { Providers } from './providers';
+import { ColorSchemeScript } from '@mantine/core';
+import { Providers } from '~/lib/Providers';
+import '@mantine/core/styles.css';
+
+export const dynamic = 'error';
 
 export default function RootLayout({ children }: PropsWithChildren) {
     return (
         <html lang="en">
+            <head>
+                <ColorSchemeScript />
+            </head>
             <body>
-                <StyledComponentsRegistry>
-                    <Providers>{children}</Providers>
-                </StyledComponentsRegistry>
+                <Providers>{children}</Providers>
             </body>
         </html>
     );
---
+++ frontend/lib/Providers.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import { PropsWithChildren } from 'react';
+import { MantineProvider } from '@mantine/core';
+import { theme } from '~/styles/theme';
+
+export const Providers: React.FC<PropsWithChildren> = ({ children }) => (
+    // TODO: uncomment when theme is filled with values
+    <MantineProvider /* theme={theme} */>{children}</MantineProvider>
+);
