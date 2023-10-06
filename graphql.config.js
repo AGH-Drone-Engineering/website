3a67a1f3903008a20aa8839d3837db9a2dcbe703
Mafciejewicz <melonik0.9846@gmail.com>
2023-10-06 23:49:28 +0200

    Fix extension config

--- graphql.config.js
+++ graphql.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-    schema: ['http://localhost:8000/wp/graphql'],
-    documents: ['./frontend/**/*.{ts|tsx}'],
+    schema: 'http://localhost:8000/index.php?graphql',
+    documents: [
+        './frontend/**/*.{ts,tsx}',
+        '!(**/node_modules/**/*)',
+    ],
 };
