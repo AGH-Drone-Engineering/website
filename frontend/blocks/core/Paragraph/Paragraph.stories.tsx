fd904a38909dcbb338c331cc8d188f909928b43e
Mafciejewicz <melonik0.9846@gmail.com>
2023-10-24 10:02:29 +0200

    Add PR checks and chromatic

---
+++ frontend/blocks/core/Paragraph/Paragraph.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ParagraphBlock as ParagraphBlockComponent } from './Paragraph';
+
+interface StoryArgs {
+    text: string;
+}
+
+const meta: Meta<StoryArgs> = {
+    component: ({ text }) => (
+        <ParagraphBlockComponent
+            block={{
+                __typename: 'CoreParagraph',
+                clientId: null,
+                parentClientId: null,
+                name: null,
+                renderedHtml: null,
+                attributes: {
+                    __typename: 'CoreParagraphAttributes',
+                    content: text,
+                },
+            }}
+        />
+    ),
+};
+
+export default meta;
+type Story = StoryObj<StoryArgs>;
+
+export const ParagraphBlock: Story = {
+    args: {
+        text: 'Lorem ipsum cośtam coś',
+    },
+    parameters: {
+        layout: 'centered',
+    },
+};
--- frontend/blocks/core/Paragraph/Paragraph.tsx
+++ frontend/blocks/core/Paragraph/Paragraph.tsx
@@ -1,6 +1,6 @@
 import { gql, TypedDocumentNode } from '@apollo/client';
 import { CoreParagraphFragment } from '~/models/graphql.generated';
-import { BlockComponent } from '../types';
+import { BlockComponent } from '../../types';
 
 export const coreParagraphFragment: TypedDocumentNode<CoreParagraphFragment> = gql`
     fragment CoreParagraph on CoreParagraph {
