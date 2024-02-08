6d5f42b9b695140b50bbdd7aff6a0563322cb1ad
Maciej Adamus <37718435+maciejewiczow@users.noreply.github.com>
2024-02-08 16:03:21 +0100

    Feature/setup custom block workflow (#7)
    Setup test custom blocks workflow
    
    ---------
    
    Co-authored-by: Maciej Adamus <maciej.adamus@fortedigital.no>

--- frontend/api/queries/getEditorBlocks.ts
+++ frontend/api/queries/getEditorBlocks.ts
@@ -1,9 +1,11 @@
 import { gql, TypedDocumentNode } from '@apollo/client';
 import { coreParagraphFragment } from '~/blocks/core/Paragraph/Paragraph';
+import { testBlockFragment } from '~/blocks/custom/TestBlock';
 import { GetEditorBlocksByUriQuery } from '~/models/graphql.generated';
 
 export const getEditorBlocksByUri: TypedDocumentNode<GetEditorBlocksByUriQuery> = gql`
     ${coreParagraphFragment}
+    ${testBlockFragment}
 
     query GetEditorBlocksByUri($uri: String!) {
         nodeByUri(uri: $uri) {
@@ -15,6 +17,7 @@ export const getEditorBlocksByUri: TypedDocumentNode<GetEditorBlocksByUriQuery> 
                     name
                     renderedHtml
                     ...CoreParagraph
+                    ...TestBlock
                 }
             }
         }
--- frontend/blocks/Fallback.tsx
+++ frontend/blocks/Fallback.tsx
@@ -2,9 +2,7 @@ import { WordPressBlockViewer } from '~/components/WordPressBlockViewer';
 import { BlockComponent } from './types';
 
 export const Fallback: BlockComponent = ({ block }) => ((block.innerBlocks?.length ?? 0) > 0 ? (
-        <div>
-            <WordPressBlockViewer blocks={block.innerBlocks ?? []} />
-        </div>
+        <WordPressBlockViewer blocks={block.innerBlocks ?? []} />
     ) : (
         <div dangerouslySetInnerHTML={{ __html: block?.renderedHtml ?? '' }} />
     ));
