79e501ad9205e8cd4a9a5a334c1d04ef9632fcf0
Mafciejewicz <melonik0.9846@gmail.com>
2024-02-09 17:05:53 +0100

    Fix wordpress url on non-local env

--- wordpress/web/app/plugins/custom-blocks/src/apiClient.ts
+++ wordpress/web/app/plugins/custom-blocks/src/apiClient.ts
@@ -1,8 +1,19 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 import possibleTypes from '../possibleTypes.json';
 
+const getUrl = () => {
+    if (window.location.host === 'localhost') {
+        return window.location.origin;
+    }
+
+    return `${window.location.origin}/${window.location.pathname
+        .split('/')
+        .slice(0, 2)
+        .join('/')}`;
+};
+
 export const client = new ApolloClient({
-    uri: `${window.location.origin}/index.php?graphql`,
+    uri: `${getUrl()}/index.php?graphql`,
     cache: new InMemoryCache({
         possibleTypes,
         typePolicies: {
